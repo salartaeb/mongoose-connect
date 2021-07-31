c48e3fbcfff7f498bc0e1a514d57c81c75a4d7bc
salartaeb <salar.taeb@hotmail.com>
2021-07-31 13:11:43 -0700

    finishing quick start

--- getting-started.js
+++ getting-started.js
@@ -5,4 +5,40 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function() {
   console.log("we're connected");
+});
+
+//defining our kitten schema
+const kittySchema = new mongoose.Schema({
+  name: String
+});
+
+kittySchema.methods.speak = function () {
+  const greeting = this.name
+    ? "Meow name is " + this.name
+    : "I don't have a name";
+  console.log(greeting);
+}
+
+
+// a model is a class in which we construct documents, 
+// in this case each document will be a kitty with properties and behaviours declared in schema.
+const Kitten = mongoose.model('Kitten', kittySchema);
+// For now we have only defined the name property which accepts a String
+// Lets create a kitty document below
+
+const silence = new Kitten({name: 'Silence'});
+// console.log(silence.name, 'this is the name of new kitty we made');
+
+
+// to save a mongodb document you need to call the save function on it
+silence.save(function (err, silence) {
+  if (err) return console.error(err);
+  silence.speak();
 })
+
+// Kitten.find((err, kittens) => {
+//   if (err) return console.log(err);
+//   console.log(kittens);
+// })
+
+Kitten.find({ name: /^Silence/ });
