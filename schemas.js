b6a519c8d9f523bf09c4c12de2225326aad3b2c2
salartaeb <salar.taeb@hotmail.com>
2021-07-31 15:06:51 -0700

    methods and static functions

--- schemas.js
+++ schemas.js
@@ -27,3 +27,30 @@ const blogSchema = new mongoose.Schema({
 // to create a model we pass it into mongoose.model(modelName, schema)
 const Blog = mongoose.model('Blog', blogSchema);
 
+//ids are added by default, you can overwrite them but that you means you also need to make sure you set it as well.
+console.log(blogSchema.path('_id')); //gives us defaulty assigned ObjectId instance of the _id property
+
+// ***************************
+
+//define a new Schema
+const animalSchema = new mongoose.Schema({ name: String, type: String});
+
+// assign a function to the methods object of our animal schema
+//now all of our animalSchema instances will have the findSimiliarTypes method available to them
+animalSchema.methods.findSimilarTypes = function(cb) {
+  return mongoose.model('Animal').find({ type: this.type }, cb);
+};
+
+
+const Animal = new mongoose.model('Animal', animalSchema)
+const dog = new Animal({type: "dog"});
+
+dog.findSimilarTypes((err, dogs) => {
+  return console.log(dogs); // woof
+})
+
+//declaring static methods with schema#static
+animalSchema.statics.findByName = function(name) {
+  if (name) return this.find({ name: new RegExp(name, 'i') });
+};
+
